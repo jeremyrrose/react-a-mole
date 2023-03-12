3a759e269ae9b88422db43e7a90047af1f51cb5d
Jeremy Rose <jeremy.r.rose@gmail.com>
2023-03-12 00:22:57 -0500

    MoleContainer behavior

--- src/components/MoleContainer.jsx
+++ src/components/MoleContainer.jsx
@@ -1,11 +1,22 @@
-// Just one example
 import Mole from './Mole'
+import EmptySlot from './EmptySlot'
+import { useState } from 'react'
 
 function MoleContainer(props){
+
+    const {score, setScore} = props
+
+    const [displayMole, setDisplayMole] = useState(false)
+
+    function bop() {
+        setScore(previous => previous + 1)
+        setDisplayMole(false)
+    }
+
     return (
         <div>
             <h2> Mole Container </h2>
-            <Mole />
+            {displayMole ? <Mole /> : <EmptySlot />}
         </div>
     )
 }
